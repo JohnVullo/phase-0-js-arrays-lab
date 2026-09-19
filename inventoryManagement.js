7fa163695ebc742ae2e0d43457aa71b4de6318bd
JohnVullo <jmv3683@gmail.com>
2026-09-18 22:31:30 -0400

    Updated inventoryManagement file for lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,42 @@
 // Write your code here
 
+//function for displaying first product of array
+function logFirstProduct(){
+  console.log(products[0])
+}
 
+//function for adding a new product name to array
+function addProduct(name){
+  products.push(name)
+  console.log(products)
+}
+
+//function for updating a name to an existing product 
+function updateProductName(position, newName){
+  products[position] = newName
+  console.log(products)
+}
+
+//removing the last product from array
+function removeLastProduct(){
+  products.pop()
+  console.log(products)
+}
+
+//defining the product array with 4 strings
+const products = ["Laptop", "Phone", "Headphone", "Monitor"]
+
+//testing for logging first product
+logFirstProduct(products)
+
+//testing for adding a new product to array
+addProduct("Mouse")
+
+//testing update for renaming a previous prodcut and getting position
+updateProductName(4, "Optical Mouse")
+
+//testing removing last product from array
+removeLastProduct()
 
 // Export the necessary parts for testing
 module.exports = {
@@ -10,3 +46,5 @@ module.exports = {
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
   products
 };
+
+
